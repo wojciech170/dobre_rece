d7c24921496e0338c7242b1f97c973e6f714f878
wojciech170 <wojciech170@gmail.com>
2024-09-10 10:36:12 +0000

    added first view with htmls and all static files

--- charity/views.py
+++ charity/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+
+
+class LandingPageView(View):
+    def get(self, request):
+        return render(request, 'charity/index.html')
+
+
+class AddDonationView(View):
+    def get(self, request):
+        return render(request, 'charity/form.html')
+
+
+class LoginView(View):
+    def get(self, request):
+        return render(request, 'charity/login.html')
+
+
+class RegisterView(View):
+    def get(self, request):
+        return render(request, 'charity/register.html')
--- dobre_rece/urls.py
+++ dobre_rece/urls.py
@@ -16,7 +16,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from charity.views import (
+    LandingPageView,
+    AddDonationView,
+    LoginView,
+    RegisterView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', LandingPageView.as_view(), name='landing'),
+    path('add_donation/', AddDonationView.as_view(), name='add_donation'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('register/', RegisterView.as_view(), name='register'),
 ]
