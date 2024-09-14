f749e95cf5f54ecac91b54852ff43d5eabf8e418
wojciech170 <wojciech170@gmail.com>
2024-09-14 11:35:13 +0000

    finished step 2 and 3 of zad: step 2 is displayed properly, added all institution from database and filtering by selected categories from step1 in js

--- charity/views.py
+++ charity/views.py
@@ -64,8 +64,10 @@ class AddDonationView(LoginRequiredMixin, View):
     login_url = 'login'
     def get(self, request):
         categories = Category.objects.all()
+        institutions = Institution.objects.all()
         ctx = {
             'categories': categories,
+            'institutions': institutions,
         }
         return render(request, 'charity/form.html', ctx)
 
